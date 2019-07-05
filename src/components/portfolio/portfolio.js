db350b23fa1afb1b7d0340ec4182dc9c971846a2
Jake <jayyf9@gmail.com>
2019-07-05 10:53:25 +0200

    implementation of first dynamic image selection.

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,32 +1,29 @@
-import React from 'react';
-import './portfolio.css';
+import React from "react";
+import "./portfolio.css";
 import portfolioData from "./portfolio-data.json";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
-
 export default function Portfolio() {
-    return(
-      <Router>
-        <Container>
-          <Row className="justify-content-md-center">
-            <Col>
-              <Link class="portfolio-item" to="/portfolioItem/ + itemId">
-                <div className="card">
-                <div className="card-image">
-                  {/* <img class="site-cover" v-bind:src="website.img"> */}
-                  A website
-                </div>
-                <div className="card-content portfolio-card-content">
-                  {/* <span class="col s9 m9 card-title">{{website.title}}</span><span class="col s3 m3"><img style="width:90px; float:right; position: relative;" :src="website.client"></span> */}
-                </div>
-              </div>
-              </Link>
-            </Col>
-          </Row>
-        </Container>
-      </Router>
+  const websites = portfolioData.map(website => {
+    return (
+      <Link className="portfolio-item" to="/portfolioItem/{website.id}">
+        <div className="card">
+          <div className="card-image">
+            <img class="site-cover" src={require('../../images/portfolio-items/multione-web/multione-web-thumbnail.png')} />
+            {website.img}
+          </div>
+          <div className="card-content portfolio-card-content">
+            <span className="card-title">{website.title}</span>
+            <img src="{website.client}" />
+          </div>
+        </div>
+      </Link>
     );
+  });
+
+  return (
+    <Router>
+      <div className="container">{websites}</div>
+    </Router>
+  );
 }
