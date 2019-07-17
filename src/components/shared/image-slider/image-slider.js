69a3b7bd156c0b2f8707e8e361fe279c5b783fc6
Jake <jayyf9@gmail.com>
2019-07-17 15:06:46 +0200

    Finished v1 of image slider

--- src/components/shared/image-slider/image-slider.js
+++ src/components/shared/image-slider/image-slider.js
@@ -2,13 +2,16 @@ import React from "react";
 import styles from "./image-slider.module.scss";
 import portfolioData from "./../../portfolio/portfolio-data";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowCircleRight, faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowCircleRight, faArrowCircleLeft, faCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default class ImageSlider extends React.Component {
     constructor() {
       super();
       this.state = { sliderNum: 0 };
     }
+
+
+
     prevImg = () => {
       if(this.state.sliderNum <= 0) { // Check if at start of images and roll over to end of true.
         this.setState({ sliderNum: portfolioData[this.props.website].siteImages.length - 1}); 
@@ -24,17 +27,32 @@ export default class ImageSlider extends React.Component {
       this.setState({ sliderNum: this.state.sliderNum + 1});
     }
 
+    switchImage = (sliderImageValue) => {
+      this.setState({ sliderNum: sliderImageValue});
+    }
+
     render() {
+      const icons = portfolioData[this.props.website].siteImages.map((ImageUrl, index) => {
+        if(index === this.state.sliderNum) {
+          return(
+            <FontAwesomeIcon onClick={() => this.switchImage(index)} className={styles.dotsSelected} icon={faCircle} size="sm" />
+          )
+        }
+        return(
+          <FontAwesomeIcon onClick={() => this.switchImage(index)} className={styles.dots} icon={faCircle} size="sm" />
+        )
+      })
+
       return (
         <div className={styles.imageSliderContainer}>
-          <img className={styles.slideImage} alt="site cover" src={portfolioData[this.props.website].siteImages[this.state.sliderNum]} />
+          <img className={styles.slideImage} alt="site cover" src={portfolioData[this.props.website].siteImages[this.state.sliderNum]} draggable="false" />
           <div className={styles.leftArrowBox} onClick={this.prevImg}>
             <FontAwesomeIcon className={styles.leftArrow} icon={faArrowCircleLeft} size="3x" />
           </div>
           <div className={styles.rightArrowBox} onClick={this.nextImg}>
             <FontAwesomeIcon className={styles.rightArrow} icon={faArrowCircleRight} size="3x" />
           </div>
-          <div className={styles.dots}>...</div>
+          {icons}
         </div>
     );
   }
